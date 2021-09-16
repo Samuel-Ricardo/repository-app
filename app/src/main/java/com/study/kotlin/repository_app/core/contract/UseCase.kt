31acc92042b0e0e89eeccf62630a23213a5fee7a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-15 23:09:02 -0300

    Create: Functions - invoke || Execute (core.contract.UseCase)

--- app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
+++ app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
@@ -4,4 +4,8 @@ import kotlinx.coroutines.flow.Flow
 
 abstract class UseCase<Param, Source> {
 
+    abstract suspend fun execute(param: Param): Flow<Source>
+
+    open suspend operator fun invoke(param: Param) = execute(param)
+
 }
