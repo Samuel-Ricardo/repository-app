4970273b10bfd05fef7f073f2faaff56a401f7fe
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-15 23:24:33 -0300

    Create: Children Class - NoSource (core.contract.UseCase)

--- app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
+++ app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
@@ -19,25 +19,8 @@ abstract class UseCase<Param, Source> {
         suspend operator fun invoke(): Flow<Source> = execute()
     }
 
+    abstract class NoSource<Params>: UseCase<Params, Unit>() {
+        override suspend fun invoke(param: Params) = execute(param)
+    }
+
 }
-
-/*
-*
-* open suspend operator fun invoke(param: Param) = execute(param)
-
-    abstract class NoParam<Source> : UseCase<None, Flow<Source>>() {
-        abstract suspend fun execute(): Flow<Source>
-
-        final override suspend fun execute(param: None) =
-            throw UnsupportedOperationException()
-
-        suspend operator fun invoke(): Flow<Source> = execute()
-    }
-
-    abstract class NoSource<Params> : UseCase<Params, Unit>() {
-        override suspend operator fun invoke(param: Params) = execute(param)
-    }
-
-    object None
-*
-* */
