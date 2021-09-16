140d3953b8ab2b8126fdb4250b454fecb3d8a3bf
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:04:10 -0300

    Create: function - load (domain.di.DomainModule)

--- app/src/main/java/com/study/kotlin/repository_app/domain/di/DomainModule.kt
+++ app/src/main/java/com/study/kotlin/repository_app/domain/di/DomainModule.kt
@@ -1,4 +1,10 @@
 package com.study.kotlin.repository_app.domain.di
 
+import org.koin.core.context.loadKoinModules
+
 object DomainModule {
+
+    fun load(){
+        loadKoinModules(useCaseModule())
+    }
 }
