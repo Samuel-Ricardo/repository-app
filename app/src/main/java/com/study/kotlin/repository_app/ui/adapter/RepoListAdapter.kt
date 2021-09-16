15e4c8e017426956540d9ce2804ba47936363a40
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 14:00:08 -0300

    Create: inner class - ViewHolder (ui.adapter.RepoListAdapter)

--- app/src/main/java/com/study/kotlin/repository_app/ui/adapter/RepoListAdapter.kt
+++ app/src/main/java/com/study/kotlin/repository_app/ui/adapter/RepoListAdapter.kt
@@ -1,7 +1,17 @@
 package com.study.kotlin.repository_app.ui.adapter
 
 import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.study.kotlin.repository_app.data.model.Repo
+import com.study.kotlin.repository_app.databinding.RepoLayoutBinding
 
 class RepoListAdapter: ListAdapter<Repo, RepoListAdapter.ViewHolder>(DiffCallback()) {
+
+    inner class ViewHolder(
+        private val binding: RepoLayoutBinding
+    ): RecyclerView.ViewHolder(binding.root){
+
+    }
+
 }
