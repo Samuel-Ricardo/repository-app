a03ed70ec1a4b38cc50656e2e10e86a8ff77e18a
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:00:25 -0300

    Override: Function - execute (domain.ListUserRepositoriesUseCase)

--- app/src/main/java/com/study/kotlin/repository_app/domain/ListUserRepositoriesUseCase.kt
+++ app/src/main/java/com/study/kotlin/repository_app/domain/ListUserRepositoriesUseCase.kt
@@ -1,4 +1,14 @@
 package com.study.kotlin.repository_app.domain
 
-class ListUserRepositoriesUseCase {
+import com.study.kotlin.repository_app.core.contract.RepoRepository
+import com.study.kotlin.repository_app.core.contract.UseCase
+import com.study.kotlin.repository_app.data.model.Repo
+import kotlinx.coroutines.flow.Flow
+
+class ListUserRepositoriesUseCase (
+    private val repository: RepoRepository
+): UseCase<String, List<Repo>>(){
+    override suspend fun execute(param: String): Flow<List<Repo>> {
+        return repository.listRepositories(param)
+    }
 }
