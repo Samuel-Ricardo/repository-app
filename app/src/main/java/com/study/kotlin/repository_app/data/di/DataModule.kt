c72d5991ab8cf669528ff6ca93764903644bb058
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 10:38:09 -0300

    Create: Function - networkModules (data.di.DataModule)

--- app/src/main/java/com/study/kotlin/repository_app/data/di/DataModule.kt
+++ app/src/main/java/com/study/kotlin/repository_app/data/di/DataModule.kt
@@ -1,15 +1,45 @@
 package com.study.kotlin.repository_app.data.di
 
+import android.util.Log
+import com.google.gson.GsonBuilder
+import okhttp3.OkHttpClient
 import org.koin.core.context.loadKoinModules
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.core.module.Module
+import org.koin.dsl.module
+import retrofit2.converter.gson.GsonConverterFactory
 
 //For Koin injection
 
 object DataModule {
 
-    private const val OK_HTTP = "OkHttp"
+    private const val OK_HTTP = "OK_HTTP"
 
     fun load() {
         loadKoinModules(networkModules() + repositoriesModule())
+    }
+
+    private fun networkModules(): Module {
+        return module{
+            single{
+                val interceptor = HttpLoggingInterceptor {
+                    Log.e(OK_HTTP, it)
+                }
+                interceptor.level = HttpLoggingInterceptor.Level.BODY
+
+                OkHttpClient.Builder()
+                    .addInterceptor(interceptor)
+                    .build()
+            }
+
+            single {
+                GsonConverterFactory.create(GsonBuilder.create());
+            }
+
+            single {
+                createService<GitHubService>(get(),get())
+            }
+        }
     }
 
 }
