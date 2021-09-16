f5e4eaf18b5241360ff2b23257ad9a37f86c00a0
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:30:46 -0300

    Create: Function - load (presentation.di.PresentationModule)

--- app/src/main/java/com/study/kotlin/repository_app/presentation/di/PresentationModule.kt
+++ app/src/main/java/com/study/kotlin/repository_app/presentation/di/PresentationModule.kt
@@ -1,6 +1,10 @@
 package com.study.kotlin.repository_app.presentation.di
 
+import org.koin.core.context.loadKoinModules
+
 class PresentationModule {
 
-
+    fun load() {
+        loadKoinModules(viewModelModule())
+    }
 }
