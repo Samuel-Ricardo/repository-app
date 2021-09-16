8d36df0d601c0093a83ab7a5fcfd9374ee71fedc
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 14:58:35 -0300

    Create: Function - setupActionBar() (ui.MainActivity)

--- app/src/main/java/com/study/kotlin/repository_app/ui/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/repository_app/ui/MainActivity.kt
@@ -27,4 +27,8 @@ class MainActivity : AppCompatActivity() {
         setupActionBar();
         binding.rvRepos.adapter = adapter;
     }
+
+    private fun setupActionBar() {
+        setSupportActionBar(binding.toolbar);
+    }
 }
