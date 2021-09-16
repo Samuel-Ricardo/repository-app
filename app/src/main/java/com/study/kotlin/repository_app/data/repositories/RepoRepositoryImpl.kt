3692447aeb74e6d309919586f29b69e8afc33ebc
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 10:56:29 -0300

    Create: Function - listRepositories (Repodata.repositories.RepoRepositoryImpl)

--- app/src/main/java/com/study/kotlin/repository_app/data/repositories/RepoRepositoryImpl.kt
+++ app/src/main/java/com/study/kotlin/repository_app/data/repositories/RepoRepositoryImpl.kt
@@ -1,9 +1,21 @@
 package com.study.kotlin.repository_app.data.repositories
 
 import com.study.kotlin.repository_app.core.contract.RepoRepository
+import com.study.kotlin.repository_app.core.exception.RemoteException
+import com.study.kotlin.repository_app.data.model.Repo
 import com.study.kotlin.repository_app.data.services.GitHubService
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class RepoRepositoryImpl(private val service: GitHubService): RepoRepository {
 
-
+    override suspend fun listRepositories(user: String): = flow {
+        try {
+            val repoList = service.listRepositories((user))
+            emit(repoList)
+        } catch (ex: HttpException) {
+            throw RemoteException(ex.message ?: "Não foi possivel fazer a busca no momento!")
+        }
+    }
 }
