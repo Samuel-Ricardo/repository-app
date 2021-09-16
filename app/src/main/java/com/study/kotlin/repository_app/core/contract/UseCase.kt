a59ad88dc293cef42820ff74776e57b0c0d68768
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-15 23:11:02 -0300

    Create: Children Class - NoParam (core.contract.UseCase)

--- app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
+++ app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
@@ -4,8 +4,15 @@ import kotlinx.coroutines.flow.Flow
 
 abstract class UseCase<Param, Source> {
 
+    object None;
+
     abstract suspend fun execute(param: Param): Flow<Source>
 
     open suspend operator fun invoke(param: Param) = execute(param)
 
+    abstract class NoParam<Source>: UseCase<None, Flow<Source>>() {
+
+    }
+
 }
+
