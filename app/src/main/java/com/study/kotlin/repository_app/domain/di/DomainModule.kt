129a919cd432890b048bdcfd4c5cc64bb3237f5e
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:06:51 -0300

    Create: function - useCaseModule (domain.di.DomainModule)

--- app/src/main/java/com/study/kotlin/repository_app/domain/di/DomainModule.kt
+++ app/src/main/java/com/study/kotlin/repository_app/domain/di/DomainModule.kt
@@ -1,10 +1,19 @@
 package com.study.kotlin.repository_app.domain.di
 
+import com.study.kotlin.repository_app.domain.ListUserRepositoriesUseCase
 import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 object DomainModule {
 
     fun load(){
         loadKoinModules(useCaseModule())
+    }
+
+    private fun useCaseModule(): Module {
+        return module{
+            factory { ListUserRepositoriesUseCase(get()) }
+        }
     }
 }
