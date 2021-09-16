d53bebc0d24544099a48ba44f0bfa7779cd816f7
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 14:53:32 -0300

    Setup: Activity - Main (ui.MainActivity)

--- app/src/main/java/com/study/kotlin/repository_app/ui/MainActivity.kt
+++ app/src/main/java/com/study/kotlin/repository_app/ui/MainActivity.kt
@@ -1,10 +1,20 @@
-package com.study.kotlin.repository_app.ui
+package com.study.kotlin.repository_app.ui;
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import com.study.kotlin.repository_app.R
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.study.kotlin.repository_app.R;
+import com.study.kotlin.repository_app.core.createProgressDialog;
+import com.study.kotlin.repository_app.databinding.ActivityMainBinding;
+import com.study.kotlin.repository_app.presentation.MainViewModel;
+import com.study.kotlin.repository_app.ui.adapter.RepoListAdapter;
+import org.koin.androidx.viewmodel.ext.android.viewModel;
 
 class MainActivity : AppCompatActivity() {
+
+    private val dialog by lazy { createProgressDialog() }
+    private val viewModel by viewModel<MainViewModel>()
+    private val adapter by lazy { RepoListAdapter() }
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
