3a004b21af4cefa5d6dfcc1d7982cba0f5b4c7aa
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:22:11 -0300

    Create: class - State (presentation)

--- app/src/main/java/com/study/kotlin/repository_app/presentation/MainViewModel.kt
+++ app/src/main/java/com/study/kotlin/repository_app/presentation/MainViewModel.kt
@@ -2,9 +2,17 @@ package com.study.kotlin.repository_app.presentation
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.study.kotlin.repository_app.data.model.Repo
 import com.study.kotlin.repository_app.domain.ListUserRepositoriesUseCase
 
 class MainViewModel(
     private val listUserRepositoriesUseCase: ListUserRepositoriesUseCase
 ): ViewModel() {
+
+
+    sealed class State {
+        object Loading: State()
+        data class Success(val list: List<Repo>): State()
+        data class Error(val error: Throwable): State()
+    }
 }
