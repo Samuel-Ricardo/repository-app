d2cc71eaa43836ab40c0ce705f7c92ca577b51dd
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:13:26 -0300

    Create: Setup - setupKoin (App)

--- app/src/main/java/com/study/kotlin/repository_app/App.kt
+++ app/src/main/java/com/study/kotlin/repository_app/App.kt
@@ -1,8 +1,28 @@
 package com.study.kotlin.repository_app
 
 import android.app.Application
+import android.app.Presentation
+import com.study.kotlin.repository_app.data.di.DataModule
+import com.study.kotlin.repository_app.domain.di.DomainModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
-class App: Application {
 
+class App: Application() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+        setupKoin();
+    }
+
+    private fun setupKoin() {
+        startKoin{
+            androidContext(this@App)
+        }
+
+        DataModule.load();
+        DomainModule.load();
+        PresentationModule.load();
+    }
 }
