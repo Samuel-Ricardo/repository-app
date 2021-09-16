c058a6e35e53c8f5869ab4e71d76eb278cbec849
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:27:02 -0300

    Create: function - getRepoList (presentation.MainViewModel)

--- app/src/main/java/com/study/kotlin/repository_app/presentation/MainViewModel.kt
+++ app/src/main/java/com/study/kotlin/repository_app/presentation/MainViewModel.kt
@@ -1,14 +1,37 @@
 package com.study.kotlin.repository_app.presentation
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.study.kotlin.repository_app.data.model.Repo
 import com.study.kotlin.repository_app.domain.ListUserRepositoriesUseCase
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     private val listUserRepositoriesUseCase: ListUserRepositoriesUseCase
 ): ViewModel() {
 
+    private val _repos = MutableLiveData<State>()
+    val repos: LiveData<State> = _repos
+
+    fun getRepoList(user: String) {
+        viewModelScope.launch {
+            listUserRepositoriesUseCase(user)
+                .onStart {
+                    _repos.postValue(State.Loading)
+                }
+                .catch {
+                    _repos.postValue(State.Error(it))
+                }
+                .collect {
+                    _repos.postValue(State.Success(it))
+                }
+        }
+    }
 
     sealed class State {
         object Loading: State()
