c131e6946025d1f8f58854a219dbc59c443247de
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 10:50:52 -0300

    Create: Method - listRepositories (core.contract.RepoRepository)

--- app/src/main/java/com/study/kotlin/repository_app/core/contract/RepoRepository.kt
+++ app/src/main/java/com/study/kotlin/repository_app/core/contract/RepoRepository.kt
@@ -1,4 +1,8 @@
 package com.study.kotlin.repository_app.core.contract
 
+import com.study.kotlin.repository_app.data.model.Repo
+import kotlinx.coroutines.flow.Flow
+
 interface RepoRepository {
+    suspend fun listRepositories(user: String): Flow<List<Repo>>
 }
