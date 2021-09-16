941d0e7ba6c556a0d742ba049b6e76abf3a8cf98
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-16 13:32:20 -0300

    Create: Function - viewModelModule (presentation.di.PresentationModule)

--- app/src/main/java/com/study/kotlin/repository_app/presentation/di/PresentationModule.kt
+++ app/src/main/java/com/study/kotlin/repository_app/presentation/di/PresentationModule.kt
@@ -1,10 +1,20 @@
 package com.study.kotlin.repository_app.presentation.di
 
+import com.study.kotlin.repository_app.presentation.MainViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.loadKoinModules
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 class PresentationModule {
 
     fun load() {
         loadKoinModules(viewModelModule())
+    }
+
+    private fun viewModelModule(): Module {
+        return module {
+            viewModel { MainViewModel(get()) }
+        }
     }
 }
