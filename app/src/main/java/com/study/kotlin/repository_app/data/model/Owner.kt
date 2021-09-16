1b3ac0ab8f40945071e2828a311690d4a3862d37
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-15 23:34:24 -0300

    Create: Model - Repo (data.model)

--- app/src/main/java/com/study/kotlin/repository_app/data/model/Owner.kt
+++ app/src/main/java/com/study/kotlin/repository_app/data/model/Owner.kt
@@ -5,5 +5,5 @@ import com.google.gson.annotations.SerializedName
 data class Owner (
     val login: String,
     @SerializedName("avatar_url")
-    val perfilImageURL: String
+    val profileImageURL: String
     )
