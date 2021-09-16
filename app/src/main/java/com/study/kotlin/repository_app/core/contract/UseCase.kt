b91f9e18ef5378406d3d045dbbc04e93c4c5df26
Samuel_Ricardo <samuelricardoofficial@gmail.com>
2021-09-15 23:22:42 -0300

    Create: Functions - invoke || Execute (core.contract.UseCase.NoParam)

--- app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
+++ app/src/main/java/com/study/kotlin/repository_app/core/contract/UseCase.kt
@@ -1,6 +1,7 @@
 package com.study.kotlin.repository_app.core.contract
 
 import kotlinx.coroutines.flow.Flow
+import java.lang.UnsupportedOperationException
 
 abstract class UseCase<Param, Source> {
 
@@ -11,8 +12,32 @@ abstract class UseCase<Param, Source> {
     open suspend operator fun invoke(param: Param) = execute(param)
 
     abstract class NoParam<Source>: UseCase<None, Flow<Source>>() {
+        abstract suspend fun execute(): Flow<Source>
 
+        final override suspend fun execute(param: None) = throw UnsupportedOperationException()
+
+        suspend operator fun invoke(): Flow<Source> = execute()
     }
 
 }
 
+/*
+*
+* open suspend operator fun invoke(param: Param) = execute(param)
+
+    abstract class NoParam<Source> : UseCase<None, Flow<Source>>() {
+        abstract suspend fun execute(): Flow<Source>
+
+        final override suspend fun execute(param: None) =
+            throw UnsupportedOperationException()
+
+        suspend operator fun invoke(): Flow<Source> = execute()
+    }
+
+    abstract class NoSource<Params> : UseCase<Params, Unit>() {
+        override suspend operator fun invoke(param: Params) = execute(param)
+    }
+
+    object None
+*
+* */
